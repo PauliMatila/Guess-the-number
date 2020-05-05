9781170400715a0ef965a87f9cca27fa6a335b8e
Pauli Matila <pauli.matila@outlook.com>
2020-05-05 10:57:30 +0300

    jottain tehty

---
+++ main.js
@@ -0,0 +1,17 @@
+function pickANumber(max)
+{
+    return Math.floor(Math.random()*max)
+}
+
+var secretNumber = pickANumber(100);
+var quessedNumbers = 0;
+
+function quessButtonPressed()
+{
+    var quessed = parseInt(document.quarySelector("#quessField").value);
+}
+
+function isValidQuess(number)
+{
+    if 
+}
